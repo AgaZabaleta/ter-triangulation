d082d01fd137bd065454ae495e2aa67dabce50e8
Xenthreaded <xenthreaded@protonmail.com>
2020-03-04 10:44:23 +0100

    Compatibilité GLSL 3.30

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QSurfaceFormat fmt;
+    fmt.setVersion( 3, 3 );
+    fmt.setProfile( QSurfaceFormat::CoreProfile );
+    QSurfaceFormat::setDefaultFormat( fmt );
     MainWindow window;
     window.show();
     return app.exec();
